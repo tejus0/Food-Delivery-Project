8d8102e33df89be67a67c1730925b15756ec75ce
Tejus <tejuschaturvedi0@gmail.com>
2023-04-28 17:36:51 +0530

    env update

--- food-app/backend/index.js
+++ food-app/backend/index.js
@@ -13,17 +13,18 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
-})
+});
 
 // json is used as we are fetching data we wrote in body of ThunderClient in CreateUser
 app.use(express.json());
 
-console.log(process.env.SECRET_KEY)
+// console.log(process.env.SECRET_KEY);
 // app.use is used as middleware in every request to server made with first param as path
 app.use("/api", require("./Routes/CreateUser"));
 app.use("/api", require("./Routes/DisplayData"));
 
-app.get("/", (req, res) => {   // if runs then express is working
+app.get("/", (req, res) => {
+  // if runs then express is working
   res.send("Hello World!    --------");
 });
 
