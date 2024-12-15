d09bf1383e6690587fd979fe5750866716f1978a
Tejus <tejuschaturvedi0@gmail.com>
2024-12-15 20:03:04 +0530

    dependency installed

--- food-app/backend/index.js
+++ food-app/backend/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const cors= require("cors")
 const express = require("express");
+const cors= require("cors")
 const app = express();
 const port = process.env.PORT ||  5000;
 
